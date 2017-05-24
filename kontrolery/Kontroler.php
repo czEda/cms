3c32a9db0b09448e6cabeb1f051c776bc1d0ad86
czEda <czeda12@gmail.com>
2017-05-24 10:42:43 +0200

    .htaccess settings

---
+++ kontrolery/Kontroler.php
@@ -0,0 +1,15 @@
+<?php
+abstract class Kontroler {
+    protected $data = array();
+    protected $pohled = "";
+    protected $hlavicka = array('titulek' => '', 'klicova_slova' => '', 'popis')
+}
+
+abstract function zpracuj($parametry);
+
+public function vypisPohled() {
+    if ($this->pohled) {
+        extract($this->data);
+        require("pohledy/" . $this->pohled . ".phtml");
+    }
+}
